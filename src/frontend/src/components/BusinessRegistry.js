a4fa974601eda377588687f4a310b797eaef8a30
addison <268240934+addisonmcs@users.noreply.github.com>
2026-04-06 03:29:29 -0400

    Sit components on MUI paper

--- src/frontend/src/components/BusinessRegistry.js
+++ src/frontend/src/components/BusinessRegistry.js
@@ -13,41 +13,49 @@ import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
+import Paper from '@mui/material/Paper';
 function BusinessRegistry() {
     // Use to make admin functions greyed out
     const [isAdmin, setIsAdmin] = useState(false);
 
     return (
-        <Box>
-            <Box sx={{display: 'flex', alignItems: 'center'}}>
-                <FormControl> 
-                <TextField label="Search for a business"></TextField>
-            </FormControl>
-            <Typography> by ...</Typography>
-            <FormControl>
-                <Select native>
-                    <option value="name">Name</option>
-                    <option value="id">ID</option>
-                    <option value="wallet">Wallet</option>
-                </Select>
-            </FormControl>
-            </Box>
-            
-            <Typography>Register a business</Typography>
-            <Box sx={{display: 'flex'}}>
-                <FormControl>
-                    <TextField label="Business Name"></TextField>
-                    <TextField label="Business ID"></TextField>
-                    <TextField label="Wallet Address"></TextField>
-                </FormControl>
-            </Box>
-            <Typography>Update business wallet address</Typography>
-            <Box sx={{display: 'flex'}}>
-                <FormControl>
-                    <TextField label="Current Wallet Address"></TextField>
-                    <TextField label="New Wallet Address"></TextField>
-                </FormControl>
-            </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, flexGrow: 1 }}>
+            <Paper sx={{ p: 3, elevation: 6 }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <FormControl>
+                        <TextField label="Search for a business"></TextField>
+                    </FormControl>
+                    <Typography> by ...</Typography>
+                    <FormControl>
+                        <Select native>
+                            <option value="name">Name</option>
+                            <option value="id">ID</option>
+                            <option value="wallet">Wallet</option>
+                        </Select>
+                    </FormControl>
+                </Box>
+            </Paper>
+
+            <Paper sx={{ p: 3, elevation: 6 }}>
+                <Typography>Register a business</Typography>
+                <Box sx={{ display: 'flex' }}>
+                    <FormControl>
+                        <TextField label="Business Name"></TextField>
+                        <TextField label="Business ID"></TextField>
+                        <TextField label="Wallet Address"></TextField>
+                    </FormControl>
+                </Box>
+            </Paper>
+
+            <Paper sx={{ p: 3, elevation: 6 }}>
+                <Typography>Update business wallet address</Typography>
+                <Box sx={{ display: 'flex' }}>
+                    <FormControl>
+                        <TextField label="Current Wallet Address"></TextField>
+                        <TextField label="New Wallet Address"></TextField>
+                    </FormControl>
+                </Box>
+            </Paper>
         </Box>
     )
 }
