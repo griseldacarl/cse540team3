900e80b24d2cd967e2b2ce715e4a144b2a4b5942
addison <268240934+addisonmcs@users.noreply.github.com>
2026-04-06 03:10:49 -0400

    minimal formatting for search

--- src/frontend/src/components/BusinessRegistry.js
+++ src/frontend/src/components/BusinessRegistry.js
@@ -9,16 +9,33 @@ import { businessRegistryABI } from '../GovernmentABI/businessRegistryABI';
 // mui imports
 import Typography from "@mui/material/Typography";
 import Box from '@mui/material/Box';
-
-
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
 function BusinessRegistry() {
     // Use to make admin functions greyed out
     const [isAdmin, setIsAdmin] = useState(false);
 
     return (
         <Box>
-            <Typography>Search for a business</Typography>
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <FormControl> 
+                <TextField></TextField>
+                <InputLabel>Search for a business</InputLabel>
+            </FormControl>
+            <Typography> by ...</Typography>
+            <FormControl>
+                <Select native>
+                    <option value="name">Name</option>
+                    <option value="id">ID</option>
+                    <option value="wallet">Wallet</option>
+                </Select>
+            </FormControl>
+            </Box>
+            
             <Typography>Register a business</Typography>
+            
             <Typography>Update business wallet address</Typography>
         </Box>
     )
